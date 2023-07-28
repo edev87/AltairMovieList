b65a4910de3ce8cf44371349474dd139238cb1fe
Ernest M. Dicks <ernest.dicks@luxinteractive.com>
2023-07-28 11:16:27 -0400

    JS code with comments about using async on function

--- js/site.js
+++ js/site.js
@@ -5,7 +5,7 @@ const API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNzlhNjJiOTJhOWNhYzQ2M2M5NzhjZD
 async function getMovies() {
   //fetch returns a promise, which promises to return data
   //await => for things that take a long time, so im going to wait for it to be done
-  //async function => might write code that will take a long time to execute
+  //async function => might write code that will take a long time to execute so we use async on function
 
   try {
     let response = await fetch('https://api.themoviedb.org/3/movie/popular',
@@ -30,7 +30,6 @@ async function displayMovies() {
   const movieListDiv = document.getElementById('movie-list');
   const moviePosterTemplate = document.getElementById('movie-card-template');
 
-
   let data = await getMovies();
 
   let movies = data.results; //movies is an array of objects
@@ -39,11 +38,46 @@ async function displayMovies() {
     let movieCard = moviePosterTemplate.content.cloneNode(true);
     let titleElement = movieCard.querySelector('.card-body > h5');
     titleElement.textContent = movie.title;
-    let descriptionElement = movieCard.querySelector('.card-body > p');
-    descriptionElement.textContent = movie.descriptionElement;
+    let descriptionElement = movieCard.querySelector('p.card-text');
+    descriptionElement.textContent = movie.overview;
+    let movieImgElement = movieCard.querySelector('.card img');
+    movieImgElement.setAttribute("src", `https://image.tmdb.org/t/p/w500${movie.poster_path}`)
+    let infoBtn = movieCard.querySelector('button.btn');
+    infoBtn.setAttribute('data-movieId', movie.id)
     movieListDiv.appendChild(movieCard);
 
   });
 
 };
 
+async function showMovieDetails(btn) {
+  let movieModal = document.getElementById('movie-modal');
+  let movieId = btn.getAttribute('data-movieId');
+  document.getElementById("movie-modal-paragraph").textContent = movieId;
+
+
+  try {
+    let response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}`,
+      {
+        headers: {
+          'Authorization': `Bearer ${API_KEY}`
+        }
+      });
+
+    let data = await response.json();
+
+    document.getElementById('modal-title').textContent = data.original_title;
+    document.getElementById('movie-modal-paragraph').textContent = data.overview;
+    document.getElementById('popularity').textContent = data.popularity;
+    document.getElementById('status').textContent = data.status;
+    document.getElementById('genres').textContent = data.genres[0].name + " " + "|" + " "
+      + data.genres[1].name + "  " + "|" + " " + data.genres[2].name;
+    document.getElementById('release-date').textContent = new Date(data.release_date).toLocaleDateString();
+    document.getElementById('movie-img').setAttribute("src", `https://image.tmdb.org/t/p/w500${data.poster_path}`)
+
+
+  } catch (error) {
+    console.error(error);
+
+  }
+}
